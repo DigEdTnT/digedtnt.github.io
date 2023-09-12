ce6119704b41fa09cda8bb243bb5ed6d3fc4fb7e
Christian Steiner <ch.stnr@gmail.com>
2023-09-12 14:06:02 +0200

    rainbow and copy button

--- assets/copyCode.js
+++ assets/copyCode.js
@@ -1,25 +1,42 @@
-// This assumes that you're using Rouge; if not, update the selector
-const codeBlocks = document.querySelectorAll('.code-header + .highlighter-rouge');
-const copyCodeButtons = document.querySelectorAll('.copy-code-button');
+document.addEventListener("DOMContentLoaded", function () {
+  // Step 1: Add a "Copy" button to each code block.
+  var codeBlocks = document.querySelectorAll(
+    ".language-xml .highlight .highlight"
+  );
+  codeBlocks.forEach(function (block) {
+    var button = document.createElement("button");
+    button.innerText = "Copy";
+    button.className = "btn btn-sm btn-secondary";
+    button.style.position = "absolute";
+    button.style.top = "5px";
+    button.style.left = "5px";
+    button.style.zIndex = 1000; // ensure it's above everything
+    button.style.opacity = "0.7"; // Set opacity for slight transparency
+    button.setAttribute("data-bs-toggle", "tooltip"); // Note the "bs" in the data attribute
+    button.setAttribute("data-bs-placement", "top");
+    button.setAttribute("title", "Code kopieren");
+    block.parentElement.style.position = "relative"; // make sure the button positions relative to the code block
+    block.parentElement.appendChild(button);
 
-copyCodeButtons.forEach((copyCodeButton, index) => {
-  const code = codeBlocks[index].innerText;
+    // Initialize Bootstrap tooltip
+    var tooltipInstance = new bootstrap.Tooltip(button);
 
-  copyCodeButton.addEventListener('click', () => {
-    // Copy the code to the user's clipboard
-    window.navigator.clipboard.writeText(code);
+    // Step 2: Attach an event listener to each button.
+    button.addEventListener("click", function () {
+      // Step 3: Copy the code inside the corresponding code block to the clipboard.
+      var code = block.textContent;
+      var textArea = document.createElement("textarea");
+      textArea.value = code;
+      document.body.appendChild(textArea);
+      textArea.select();
+      document.execCommand("copy");
+      document.body.removeChild(textArea);
 
-    // Update the button text visually
-    const { innerText: originalText } = copyCodeButton;
-    copyCodeButton.innerText = 'Kopiert!';
-
-    // (Optional) Toggle a class for styling the button
-    copyCodeButton.classList.add('copied');
-
-    // After 2 seconds, reset the button to its initial UI
-    setTimeout(() => {
-      copyCodeButton.innerText = originalText;
-      copyCodeButton.classList.remove('copied');
-    }, 2000);
+      // Update the tooltip title and show it
+      tooltipInstance.dispose(); // dispose the previous tooltip instance
+      button.setAttribute("title", "In die Zwischenablage kopiert");
+      tooltipInstance = new bootstrap.Tooltip(button); // create a new tooltip with the updated title
+      tooltipInstance.show();
+    });
   });
 });
